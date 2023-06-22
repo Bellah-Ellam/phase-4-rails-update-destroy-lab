d3ad9ef6929f26087e5c57b91c16845b0eee5d3c
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-06-22 23:47:29 +0300

    complete lab

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,4 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
     plants = Plant.all
@@ -16,6 +15,24 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant 
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: plant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
   end
 
   private
